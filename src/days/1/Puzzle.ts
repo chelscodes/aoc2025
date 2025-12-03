58b80109d5858e31caf55cad8ab059c45197a48f
chelscodes <chelsea@inviteeducation.com>
2025-12-02 19:04:36 -0500

    day 1 part 2

--- src/days/1/Puzzle.ts
+++ src/days/1/Puzzle.ts
@@ -6,7 +6,18 @@
 // 4. move in correct direction (init at 50), save end position and console.log
 // 4a. if movement passes 0 or 99, adjust accordingly
 
-// const testInput = ["R50", "L25", "R21", "L50", "R13", "L59"];
+// const testInput = [
+//   "L68",
+//   "L30",
+//   "R48",
+//   "L5", // not working
+//   "R60",
+//   "L55",
+//   "L1",
+//   "L99",
+//   "R14",
+//   "L82",
+// ];
 
 const normalizeDistance = (distance: number) => {
   let newDistance = distance;
@@ -51,11 +62,61 @@ const first = (input: string) => {
 };
 const expectedFirstSolution = "1023";
 
-const second = (input: string) => {
-  // console.log(input);
-  return "solution 2";
+const getDistanceAmounts = (distance: number) => {
+  let newDistance = distance;
+  let rotations = 0;
+  if (distance === 100) {
+    newDistance = 0;
+    rotations = 1;
+  } else if (distance > 100) {
+    newDistance = distance % 100;
+    rotations = Math.floor(distance / 100);
+  }
+  return { distance: newDistance, rotations };
 };
 
-const expectedSecondSolution = "solution 2";
+const second = (input: string) => {
+  const array = input.split(/\r?\n/);
+  let currentPosition = 50;
+  let landsZero = 0;
+  let spinsPastZero = 0;
+  let adjustedPastZero = 0;
+
+  for (const item of array) {
+    const direction = item.match(/[LR]/)?.[0];
+
+    const { distance, rotations } = getDistanceAmounts(
+      Number(item.substring(1))
+    );
+
+    let newPosition = currentPosition;
+    if (direction === "R") {
+      newPosition = currentPosition + distance;
+      if (newPosition > 99) {
+        newPosition = newPosition - 100;
+        if (newPosition !== 0) {
+          adjustedPastZero++;
+        }
+      }
+    } else {
+      newPosition = currentPosition - distance;
+      if (newPosition < 0) {
+        newPosition = 100 + newPosition;
+        if (newPosition !== 0 && currentPosition !== 0) {
+          adjustedPastZero++;
+        }
+      }
+    }
+
+    spinsPastZero = spinsPastZero + rotations;
+    if (newPosition === 0) {
+      landsZero++;
+    }
+    currentPosition = newPosition;
+  }
+  return landsZero + spinsPastZero + adjustedPastZero;
+};
+
+const expectedSecondSolution = "5899";
 
 export { expectedFirstSolution, expectedSecondSolution, first, second };

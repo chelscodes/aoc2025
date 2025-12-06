8fe7e6bed82d21c384c1e157d91971289c122649
chelscodes <chelsea@inviteeducation.com>
2025-12-06 14:45:10 -0500

    add comment

--- src/utils/inputToArray.ts
+++ src/utils/inputToArray.ts
@@ -2,6 +2,7 @@ export const inputToArrayLines = (input: string) => {
   return input.split(/\r?\n/);
 };
 
+// when there's a blank line that separates type of data
 export const inputToArrayLineSections = (input: string) => {
   const firstSplit = input.split(/\r?\n\n/)
   const multipleArrays = firstSplit.map((string) => {

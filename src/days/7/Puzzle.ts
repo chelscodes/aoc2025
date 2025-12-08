f7725f2b24a89a926e81db8c74da5883f5437037
chelscodes <chelsea@inviteeducation.com>
2025-12-07 22:47:56 -0500

    save progress

--- src/days/7/Puzzle.ts
+++ src/days/7/Puzzle.ts
@@ -1,31 +1,35 @@
 import { inputToArrayLines } from "../../utils/inputToArray.ts";
 
+const getSplitterLocations = (currentLine: string) => {
+  const splitters: number[] = []
+  // look for index of splitters
+  let found = true
+  let lastSplitterIndex = 0
+  while (found) {
+    const splitterIndex = currentLine.indexOf("^", lastSplitterIndex)
+
+    if (splitterIndex !== -1) {
+      splitters.push(splitterIndex)
+      lastSplitterIndex = splitterIndex + 1
+    } else {
+      found = false
+    }
+  }
+  
+  return splitters
+}
+
 const first = (input: string) => {
   const data = inputToArrayLines(input)
 
-  const length = data.length // amount of lines to check
-  // const width = data[0].length
   const startBeam = data[0].indexOf("S") 
   let currentBeams: number[] = [startBeam]
   let countOfSplits = 0
-  // look two lines down, if beam hits a splitter, beam continues at index - 1 and index + 1
-  for (let i = 2; i < length; i += 2) {
-    const currentLine = data[i]
-    const splitters: number[] = []
-    // look for index of splitters
-    // const index = currentLine.indexOf("^")
-    let found = true
-    let lastSplitterIndex = 0
-    while (found) {
-      const splitterIndex = currentLine.indexOf("^", lastSplitterIndex)
 
-      if (splitterIndex !== -1) {
-        splitters.push(splitterIndex)
-        lastSplitterIndex = splitterIndex + 1
-      } else {
-        found = false
-      }
-    }
+  // look two lines down, if beam hits a splitter, beam continues at index - 1 and index + 1
+  for (let i = 2; i < data.length; i += 2) {
+    const currentLine = data[i]
+    const splitters = getSplitterLocations(currentLine)
 
     // compare: did beam hit the splitter
     // if yes, increase countOfSplits, replace hit beam with index - 1, index + 1
@@ -38,15 +42,7 @@ const first = (input: string) => {
         currentBeams.splice(matchIndex, 1, splitterIndex -1, splitterIndex +1)
       }
     })
-
-    // remove duplicate beam indices
-    const filteredBeams = currentBeams.filter((item, index, array) => item !== array[index + 1]);
-    currentBeams = filteredBeams
-
-    // console.log({currentLine, splitters, currentBeams, filteredBeams, index})
   }
-
-  // console.log({ width })
   return countOfSplits;
 };
 
@@ -54,11 +50,44 @@ const first = (input: string) => {
 const expectedFirstSolution = 1711;
 
 const second = (input: string) => {
-  // console.log(input);
-  return 'solution 2';
+  const data = inputToArrayLines(input)
+
+  const startBeam = data[0].indexOf("S") 
+  let currentBeams: number[] = [startBeam]
+  let countOfPaths = 1
+
+  for (let i = 2; i < data.length; i += 2) {
+    console.log(i)
+    const splitters = getSplitterLocations(data[i])
+  
+    let newBeams: number[] = []
+
+    // console.log("Line: ", i + 1, { splitters, currentBeams})
+    
+    currentBeams.forEach((beamLocation, i) => {
+      const matchCheck = splitters.includes(beamLocation)
+
+      if (matchCheck) {
+        countOfPaths++
+
+        newBeams.push(beamLocation - 1)
+        newBeams.push(beamLocation + 1)
+        newBeams.sort((a, b) => a - b)
+      } else {
+        newBeams.push(beamLocation)
+      }
+      // console.log({i, beamLocation, matchCheck})
+    })
+
+    currentBeams = newBeams
+  }
+
+  console.log({countOfPaths, currentBeams, length: currentBeams.length})
+
+  return countOfPaths;
 };
 
-// example answer =
+// example answer = 40
 const expectedSecondSolution = 'solution 2';
 
 export { expectedFirstSolution, expectedSecondSolution, first, second };

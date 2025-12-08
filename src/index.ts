a7850c7fbf79a169af3d05cbc9872b8b475d3f64
chelscodes <chelsea@inviteeducation.com>
2025-12-08 12:02:21 -0500

    add time logs

--- src/index.ts
+++ src/index.ts
@@ -28,5 +28,11 @@ const { first, second }: Puzzle = await import(
   `./days/${puzzleName}/Puzzle.ts`
 );
 
+console.time("processing 1")
 console.log(first(input));
+console.timeLog("processing 1")
+
+console.time("processing 2")
 console.log(second(input));
+console.timeLog("processing 2")
+
